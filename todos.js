19edc41cc28c7921a57424701c4e5884d4d3f9ce
Leandro Vieira <leandrown@outlook.com>
2020-01-06 12:46:05 -0300

    Estrutura do App e iniciando a aplicação

---
+++ todos.js
@@ -0,0 +1,10 @@
+let listElement = document.querySelector('#app ul');
+let inputElement = document.querySelector('#app input');
+let buttonElement = document.querySelector('#app button');
+
+let todos = [
+    'Fazer café',
+    'Estudar JavaScript',
+    'Estudar C# e ASP.NET Core',
+    'Acessar comunidade da Rocketseat'
+];
